77f5d665c71505bd88a61898145cc10f3586ff82
Shubham Sinha <hsinha177@gmail.com>
2019-07-28 13:09:55 +0530

    Update userdb.py

--- backend/userdb.py
+++ backend/userdb.py
@@ -19,18 +19,9 @@ p.close()
 
 newtable = name+label
 
-connect=mysql.connector.connect(host="localhost",user="root",passwd="root",database="reko")
+connect=mysql.connector.connect(host='localhost',user='username',passwd='user password',database='database name')
 mycur=connect.cursor()
 #mycur.execute("insert into %s(imageid,sentiment) values('%s','%s')"%(name,imageid,label))
 mycur.execute("update %s set imageid='%s', sentiment='%s' where login_time='%s'"%(name,imageid,label,login_time))
 connect.commit()
 connect.close()
-
-#mydb=mysql.connector.connect(host="localhost", user="root",passwd="root",database="reko")
-#mycursor=mydb.cursor()
-#mycursor.execute("CREATE TABLE IF NOT EXISTS %s(login_time varchar(20),songname varchar(20))"%(newtable))
-#mydb.commit()
-#print("user signup successful. ':)'")
-#mydb.close()
-
-#print("<meta http-equiv='refresh' content='0;url=https://13.233.28.221/index.php'>")
