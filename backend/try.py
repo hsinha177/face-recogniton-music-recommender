1a843abc540a9a5c5f225e3c9c05435983fe15be
Shubham Sinha <hsinha177@gmail.com>
2019-07-28 13:11:13 +0530

    Update try.py

--- backend/try.py
+++ backend/try.py
@@ -8,11 +8,11 @@ recipient=input("Enter your email : ")
 name = recipient.split("@")[0]
 print(name)
 
-mydb=mysql.connector.connect(host="localhost", user="root",passwd="root",database="reko")
+mydb=mysql.connector.connect(host='localhost',user='username',passwd='user password',database='database name')
 mycursor=mydb.cursor()
 mycursor.execute("create table %s(uname varchar(20),login_time varchar(20),logout_time varchar(20),sentiment varchar(20))"%(name))
 mydb.commit()
-conn=mysql.connector.connect(host="localhost",user="root",passwd="root",database="reko")
+conn=mysql.connector.connect(host='localhost',user='username',passwd='user password',database='database name')
 mycursor=conn.cursor()
 mycursor.execute("insert into %s(uname,login_time) values('%s','%s')"%(name,name,ctime))
 conn.commit()
